250fa0800957bc937154855c9a2df6fb02fc67d7
Chibuezem Marvinrose <rozzeymarvin32@gmail.com>
2023-05-12 12:34:07 +0100

    Created a CRUD app and added its contents

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const express = require("express");
 
 const expressLayout = require("express-ejs-layouts");
 
+const connectDB = require("./server/config/db");
+
 const app = express();
 
 const port = 5000 || process.env.PORT;
+
+// connect to database
+connectDB();
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -20,15 +25,8 @@ app.use(expressLayout);
 app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 
-// Home
-app.get("/", (req, res) => {
-  const locals = {
-    title: "CRUD",
-    description: "User Management Sysytem",
-  };
-
-  res.render("index", locals);
-});
+// Routes
+app.use("/", require("./server/routes/customer"));
 
 // handle 404
 app.get("*", (req, res) => {
---
+++ server/controllers/customerController.js
@@ -0,0 +1,65 @@
+const customer = require("../models/customer");
+
+const mongoose = require("mongoose");
+
+// GET
+
+// Homepage
+
+exports.homepage = async (req, res) => {
+  // Home
+
+  const locals = {
+    title: "User Management Sysytem",
+    description: "User Management Sysytem Using Nodejs",
+  };
+
+  res.render("index", locals);
+};
+
+// GET
+
+//New customner form
+
+exports.addCustomer = async (req, res) => {
+  // Home
+
+  const locals = {
+    title: "Add New Customer - CRUD",
+    description: "User Management Sysytem",
+  };
+
+  res.render("customer/add", locals);
+};
+
+// POST
+
+//Create New customer form
+
+exports.postCustomer = async (req, res) => {
+  console.log(req.body);
+
+  const newCustomer = new customer({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    details: req.body.details,
+    tel: req.body.tel,
+    email: req.body.email,
+  });
+
+  try {
+    await customer.create(newCustomer);
+    // await req.flash("info", "New customer has been added.");
+
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
+  }
+
+  // const locals = {
+  //   title: "New Customer Added!",
+  //   description: "User Management Sysytem",
+  // };
+
+  // res.render("customer/add", locals);
+};
